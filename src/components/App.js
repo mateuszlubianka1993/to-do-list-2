cee78a0a815bac9074ed74dadc0d2cdf1bb7c846
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-05-08 13:16:19 +0200

    add ToDoForm component and ToDoForm.css file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Header from './Header';
+import ToDoForm from './ToDoForm';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Header />
+                <ToDoForm />
             </div> 
         );
     };
---
+++ src/components/ToDoForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MDBFormInline, MDBBtn } from "mdbreact";
+import './style/ToDoForm.css';
+
+const ToDoForm = () => {
+    return (
+        <div className="form-container">
+            <MDBFormInline className="md-form mr-auto mb-4 search-box">
+                <input className="form-control mr-sm-2"  type="text" placeholder="Add next task" aria-label="Search" />
+                <MDBBtn rounded size="sm" color="success">Add task</MDBBtn>
+            </MDBFormInline>
+            <MDBBtn rounded color="danger" size="sm">Delete All</MDBBtn>
+            <MDBBtn rounded color="danger" size="sm">Delete Last</MDBBtn>
+        </div>
+    );
+};
+
+export default ToDoForm;
