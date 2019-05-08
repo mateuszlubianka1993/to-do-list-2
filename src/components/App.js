eef9436cc580fbb8c892e6152149cba9b3de6722
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-05-08 13:22:06 +0200

    add onInputChange function

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,24 @@ import Header from './Header';
 import ToDoForm from './ToDoForm';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: [],
+            inputValue: ''
+          };
+      };
+
+      onInputChange = (e) => {
+        //   console.log(e.target.value);
+        this.setState({inputValue: e.target.value})
+      };
+
     render() {
         return (
             <div>
                 <Header />
-                <ToDoForm />
+                <ToDoForm onInputChange={this.onInputChange}/>
             </div> 
         );
     };
--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { MDBFormInline, MDBBtn } from "mdbreact";
 import './style/ToDoForm.css';
 
-const ToDoForm = () => {
+const ToDoForm = (props) => {
     return (
         <div className="form-container">
             <MDBFormInline className="md-form mr-auto mb-4 search-box">
-                <input className="form-control mr-sm-2"  type="text" placeholder="Add next task" aria-label="Search" />
+                <input onChange={props.onInputChange} className="form-control mr-sm-2"  type="text" placeholder="Add next task" aria-label="Search" />
                 <MDBBtn rounded size="sm" color="success">Add task</MDBBtn>
             </MDBFormInline>
             <MDBBtn rounded color="danger" size="sm">Delete All</MDBBtn>
